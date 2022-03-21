9e0d5a2d29c56352269695daea3c89edc862ad76
kapka videnova <videnovakapka@gmail.com>
2022-03-21 09:47:01 +0200

    Update version to 6.0.1

--- Web/BabyGet.Web/Startup.cs
+++ Web/BabyGet.Web/Startup.cs
@@ -67,7 +67,6 @@
             services.AddTransient<ISettingsService, SettingsService>();
             services.AddTransient<IGetCountsService, GetCountsService>();
             services.AddTransient<ICategoriesService, CategoriesService>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
